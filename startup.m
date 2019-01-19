b43eaf8e3a3a77485ae5558ac00143a8c19aae86
Riccardo Bertoglio <18362950+ricber@users.noreply.github.com>
2019-01-19 16:41:10 +0100

    Complete project

---
+++ startup.m
@@ -0,0 +1,8 @@
+% these file is executed by matlab when it starts if the initial working
+% directory is the one containing this file. This file is useful to add
+% directories as in the path 
+
+addpath src
+addpath src/utils
+addpath src/utils/arrow3
+addpath imgs
